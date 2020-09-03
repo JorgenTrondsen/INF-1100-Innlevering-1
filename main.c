5aa41832512c047b9f185744be92b813af215e9c
Jørgen Trondsen <43483104+JorgenTrondsen@users.noreply.github.com>
2020-09-03 13:17:38 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -14,6 +14,7 @@ void compare_values(int x, int y)
         printf("%d", x);
     }
     printf("\n");
+    printf("\n");
 }
 
 // Print a triangle with the size of numlines
@@ -27,19 +28,50 @@ void triangle(int numlines)
         }
         printf("\n");
     }
+    printf("\n");
 }
 
 // Determine whether the input number have a given prime factor.
 int my_prime_factor(int number, int prime_factor)
 {
-    // TODO: Complete this
+    int result; 
+    result = number % prime_factor; {
+        
+        if (result == 0) {
+
+            return 1;
+        }
+
+        else if (result != 0) {
+
+            return 0;
+        }
+    }
 }
 
 // Write a C function that prints numbers in a given range, 
 // along with an indication of whether the number is odd or even and whether 5 is a prime factor in the numbers
 void prime_factor_range(int start, int end)
 {
-    // TODO: Complete this
+    for(start; start <= end; start++) 
+    {
+        int OddEven = my_prime_factor(start, 2);
+        
+        if(OddEven == 0) {
+            printf("%d is odd, ", start);
+        }
+
+        else printf("%d is even, ", start);
+
+        int IsPrime = my_prime_factor(start, 5);
+
+        if (IsPrime == 1) {
+            printf("and is a prime factor of 5\n");
+        }
+
+        else printf("and is not a prime factor of 5\n");
+    }
+    printf("\n");
 }
 
 // Calculates log2 of an unsigned integer n using the following approach: 
@@ -47,7 +79,15 @@ void prime_factor_range(int start, int end)
 // For example, if n is 17 (0b10001), the function should return 4. 
 void my_log2(unsigned int num)
 {
-    // TODO: Complete this
+       int Bitposition = -1;
+
+    while (num > 0)
+    {
+        num = num >> 1;
+
+        Bitposition ++;
+    }
+    printf("The most significant bit is in position %d", Bitposition);
 }
 
 
@@ -56,9 +96,9 @@ int main()
     // Try changing these values
     compare_values(69, 88);
     triangle(5);
-    my_prime_factor(24, 3);
+    my_prime_factor(24, 2);
     prime_factor_range(23, 29);
-    my_log2(10);
+    my_log2(17);
 
     return EXIT_SUCCESS;
 }
